0030dfc746bfe5c053440a4b119333cc3bb9248a
ethan <ethan@grif.us>
2019-03-04 20:52:56 -0700

    the board is made final

--- MasteryTCG/src/Board.java
+++ MasteryTCG/src/Board.java
@@ -4,65 +4,18 @@ public class Board {
 	static int height = 5;
 	static int width = 5;
 
-	static Card[][][] board = new Card[height][width][2];
+	static Card[][] board = new Card[height][width];
 
 	public static void addCard(Card card, int x, int y) {
-		if (card instanceof Trap == false && board[x][y][0] != null) {
-			board[x][y][0] = card;
-		} else if (card instanceof Trap && board[x][y][1] != null) {
-			board[x][y][1] = card;
-
-		}
+		if ( board[x][y] == null) {
+			board[x][y] = card;
+		} 
 
 	}
 
-	public static Card[] getCards(int x, int y) {
-		int state = 1;
-		if (board[x][y][0] != null) {
-			// if there's a Hero, *2
-			state = state * 2;
-		}
-		if (board[x][y][1] != null) {
-			// if there's a trap *3
-			state = state * 3;
-		}
-
-		// heres to explain why I did that weird multiplying thing. if there's a need
-		// for a third layer in the board, it will be a lot easier to make new
-		// conditionals. If every condition multiplies the State by a unique prime
-		// number, every combo of those conditionals will make a unique product, which
-		// is easy to check for in a switch. [End gibberish]
-
-		Card[] Return;
-
-		// P.S. there's probably a better way to what i did here, I really didn't want
-		// to
-		// use one-time use vars, but whatevs :/
-		switch (state) {
-
-		case (2 * 1):
-			// a Hero but no Trap
-			Card[] temp = { board[x][y][0] };
-			Return = temp;
-			break;
-
-		case (1 * 3):
-			// a Trap but no Hero
-			Card[] temp2 = { board[x][y][1] };
-			Return = temp2;
-			break;
-
-		case (2 * 3):
-			// both a Hero and Trap
-			Card[] temp3 = { board[x][y][0], board[x][y][1] };
-			Return = temp3;
-			break;
-
-		default:
-			// none of the above
-			Return = null;
-			break;
-		}
+	public static Card getCards(int x, int y) {
+		Card Return;
+		Return = board[x][y];
 
 		return Return;
 
