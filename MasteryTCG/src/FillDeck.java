6738b5c18a9fb858620af746a1481656693fdd41
jeffer.ng <jeffer.ng@MU-C1-14.mullen.pvt.k12.co.us>
2019-03-18 09:10:30 -0600

    added name adding in player

--- MasteryTCG/src/FillDeck.java
+++ MasteryTCG/src/FillDeck.java
@@ -8,12 +8,12 @@ public class FillDeck
 		//This Method Is The Runner Of This Class
 		public static Player fillDeck() throws IOException
 		{
-			TestRunner.printOut("GREETINGS! WHAT IS YOUR NAME?", 50);
+			//TestRunner.printOut("GREETINGS! WHAT IS YOUR NAME?", 50);
 			
-			Scanner userInput = new Scanner (System.in);
-			String name = userInput.nextLine();
+//			Scanner userInput = new Scanner (System.in);
+//			String name = userInput.nextLine();
 			
-			TestRunner.printOut("WELCOME, " + name.toUpperCase() + "!", 50);
+			//TestRunner.printOut("WELCOME, " + name.toUpperCase() + "!", 50);
 			
 			ArrayList<Card> deck = askUserWhichDeck();
 			Player p = new Player();
